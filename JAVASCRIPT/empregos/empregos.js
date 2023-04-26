242e12fc7a9bdd900deee4b670521f289518160d
Vitor <vitorsouzasilva121@gmail.com>
2023-04-26 15:00:42 -0300

    fit: add exercise

---
+++ JAVASCRIPT/empregos/empregos.js
@@ -0,0 +1,26 @@
+function Menu(){
+    opcao = prompt("Escolha uma das opçoes: \n"+
+    "1- Listar  vagas diponiveis\n"+
+    "2- Criar uma vaga\n"+
+    "3- Visualizar uma vaga\n"+
+    "4- Inscrever um candidato em uma vaga\n"+
+    "5- Excluir uma vaga\n"+
+    "6- Sair")
+    return opcao
+}
+
+const vagas= {}
+
+function listar(){
+    alert({
+        indice,
+        nome,
+        quantidade
+    })
+}
+
+vagas.forEach(listar)
+
+function Criar(){
+    nome = prompt()
+}
